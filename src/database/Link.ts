07689a6266bea88375ac34bd63a46469c8b9bea3
Antonio Vitor <antoniovitorFBN@gmail.com>
2020-07-25 09:14:55 -0300

    Adds data scraping and error handling to bookInfo

--- src/database/Link.ts
+++ src/database/Link.ts
@@ -3,21 +3,38 @@ import { Document, DocumentSchema } from 'camo'
 interface Schema extends DocumentSchema {
     URL: string
     pdfURL?: string
+    authors?: string
+    bookName?: string
     status: 'created'
         | 'scraped'
         | 'downloaded'
         | 'error-scrap'
         | 'error-download'
+    errors: []
 }
 
 class Link extends Document<Schema> {
+    URL: string
+    pdfURL?: string
+    authors?: string
+    bookName?: string
+    status: 'created'
+        | 'scraped'
+        | 'downloaded'
+        | 'error-scrap'
+        | 'error-download'
+    errors: []
+
     constructor () {
         super()
 
         this.schema({
             URL: String,
             pdfURL: String,
-            status: String
+            status: String,
+            authors: String,
+            bookName: String,
+            errors: []
         })
     }
 }
--- src/spiders/bookInfo.ts
+++ src/spiders/bookInfo.ts
@@ -1,16 +1,54 @@
 import { SpiderInterface } from '../module/spider'
 import Link from '../database/Link'
-// import axios from 'axios'
-// import cheerio from 'cheerio'
+import axios from 'axios'
+import cheerio from 'cheerio'
+import fs from 'fs'
 
 const BookInfoSpider: SpiderInterface<Link> = {
+    maxParallel: 2,
+
     data: async () => {
-        return Link.find({})
+        return (await Link.find<Link>({})).slice(0, 1)
     },
+
     execute: async (link) => {
-        console.log(`Start scraping ${link._id}.`)
+        console.log(`Running link ${link.URL}`)
+        const response = await axios.get(link.URL.toString())
+        const scraper = cheerio.load(response.data)
+
+        let errors: [] = []
+
+        /**
+         * Extracts PDF's link
+         */
+        const relativePdfURL = scraper('.test-bookpdf-link').attr('href')
+        if (relativePdfURL) {
+            link.pdfURL = `http://link.springer.com${relativePdfURL}`
+        } else {
+            errors.push('pdf-link-not-found')
+        }
+
+        /**
+         * Extract authors names
+         */
+        link.authors = scraper('span.authors__name').map(
+            (index, elem) => scraper(elem).text()
+        ).get().join('; ')
+
+        /**
+         * Extracts book name
+         */
+        link.bookName = scraper('div.page-title > h1').text()
+
+        link.errors = errors
+
+        link.save()
     },
-    maxParallel: 4
+
+    catchError: (err, link) => {
+        console.log(`An error has occured in link: ${link.URL}`)
+        console.error(err)
+    }
 }
 
 export default BookInfoSpider
